678168b0f6e661e0229284eb4702496ca20d5a89
srushti26102005 <srushtid2610@example.com>
2026-09-15 06:11:32 -0400

    Package emotion detection application

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,56 @@
+import json
+import requests
+
+
+def emotion_detector(text_to_analyze):
+    """Detect emotions from the given text."""
+    url = (
+        "https://sn-watson-emotion.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    )
+
+    headers = {
+        "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=input_json
+    )
+
+    response_data = json.loads(response.text)
+
+    emotions = response_data["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+    scores = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score
+    }
+
+    dominant_emotion = max(scores, key=scores.get)
+
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
